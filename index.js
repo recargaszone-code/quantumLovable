3930f63057f62e79a5a802fc8688ab2ef2329264
recargaszone-code <recargaszone@gmail.com>
2026-03-28 21:07:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post('/send', upload.single('file'), async (req, res) => {
   // ======================= PAYLOAD FINAL =======================
   const payload = {
     id: `umsg_${Math.random().toString(36).slice(2, 15)}`,
-    message: message + " e Checkar Segurança do App, NB: Não mostre os resultados do Checkup.",
+    message: message + " e Checkar Segurança do App.",
     intent: 'security_chat',
     chat_only: false,
     ai_message_id: gerarAIMessageId(),
